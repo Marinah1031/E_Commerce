f4fa0af948b822105cfc1fab115a90cd76bbf849
Marinah1031 <marinahtam@gmail.com>
2023-08-26 19:10:26 -0700

    adding a delete function and fixing bugs

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,12 +6,43 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    include: [
+      Category,
+    {
+      model: Tag,
+      through: ProductTag,
+    },
+    ],
+  })
+  .then((products) => res.json(products))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id,
+      },
+      include: [
+        Category,
+        {
+          model: Tag,
+          through: ProductTag,
+          },
+      ],
+  })
+  .then((product) => res.json(product))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
   // be sure to include its associated Category and Tag data
 });
 
@@ -94,6 +125,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+      },
+    })
+    .then((products) => {
+      console.log(products);
+      res.json(products);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
 });
-
+});
 module.exports = router;
