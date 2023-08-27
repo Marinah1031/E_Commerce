ca8abaf3919c66f3b26caa488ad54948a1cee88c
Marinah1031 <marinahtam@gmail.com>
2023-08-26 19:13:46 -0700

    adding finding all tage and endpoints and including its associated product data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll({
+    include: [
+      { 
+        model: Product, 
+        through: ProductTag,
+      },
+    ],
   // be sure to include its associated Product data
+})
+  .then((tags) => res.status(200).json(tags))
+  .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
